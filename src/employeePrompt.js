eec575a44e948c467dfb6206d3f57ff5f437b422
Steven Israel <malenchite@gmail.com>
2021-02-22 10:27:38 -0500

    Initial action prompts and function stubs

--- src/employeePrompt.js
+++ src/employeePrompt.js
@@ -1,15 +1,81 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-const employeeQuestions = [
-
+/* Inquirer questions for basic actions */
+const actionQuestions = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    name: "action",
+    choices: [
+      "View Departments",
+      "View Roles",
+      "View Employees",
+      "Add Department",
+      "Add Role",
+      "Add Employee",
+      "Exit"
+    ]
+  }
 ];
 
+/* Displays all departments */
+function viewDepartments(conn) {
+  console.log("Departments");
+}
+
+/* Displays all roles or by department, as chosen by user*/
+function viewRoles(conn) {
+  console.log("Roles");
+}
+
+/* Displays all employees or by department, as chosen by user*/
+function viewEmployees(conn) {
+  console.log("Employees");
+}
+
+/* Add a department */
+function addDepartment(conn) {
+  console.log("Add department");
+}
+
+/* Add a role */
+function addRole(conn) {
+  console.log("Add role");
+}
+
+/* Add an employee */
+function addEmployee(conn) {
+  console.log("Add employee");
+}
 
 function employeePrompt(conn) {
-  return inquirer.prompt(employeeQuestions)
+  return inquirer.prompt(actionQuestions)
     .then(answers => {
-
+      /* Drop out of then() if Exit is chosen */
+      if (answers.action != "Exit") {
+        switch (answers.action) {
+          case "View Departments":
+            viewDepartments(conn);
+            break;
+          case "View Roles":
+            viewRoles(conn);
+            break;
+          case "View Employees":
+            viewEmployees(conn);
+            break;
+          case "Add Department":
+            addDepartment(conn);
+            break;
+          case "Add Role":
+            addRole(conn);
+            break;
+          case "Add Employee":
+            addEmployee(conn);
+            break;
+        }
+        return employeePrompt(conn);
+      }
     });
 }
 
