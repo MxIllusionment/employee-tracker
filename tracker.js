1bdf43a526b726300dbf5d6917ed74c1d3087f01
Steven Israel <malenchite@gmail.com>
2021-02-22 19:08:34 -0500

    Refactored DB start to be explicit and use a promise

--- tracker.js
+++ tracker.js
@@ -1,18 +1,23 @@
 const Database = require("./lib/Database");
 const actionPrompt = require("./src/actionPrompt");
 
-/* Start up database connec tion */
+/* Start up database connection */
 const db = new Database();
 
-/* Initial startup display */
-console.log("Welcome to the Employee Tracker!");
-console.log("--------------------------------");
-
-/* Process inquirer questions */
-actionPrompt(db)
+db.start()
   .then(() => {
+    /* Initial startup display */
+    console.log("Welcome to the Employee Tracker!");
     console.log("--------------------------------");
-    console.log("Exiting Employee Tracker. Farewell!");
-    db.exit();
+
+    /* Process inquirer questions */
+    actionPrompt(db)
+      .then(() => {
+        console.log("--------------------------------");
+        console.log("Exiting Employee Tracker. Farewell!");
+        db.exit();
+      });
   });
+
+
 
