4b2c9343f166f45b63a77b2a56843c6606c393e9
Steven Israel <malenchite@gmail.com>
2021-02-23 11:20:21 -0500

    Added data to seed

--- seed.sql
+++ seed.sql
@@ -37,6 +37,7 @@ CREATE TABLE employee (
 -- Example starting data --
 INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Support");
 
 INSERT INTO role (title, salary, department_id)
     VALUES ("Salesperson", 40000.00, 1);
@@ -47,6 +48,11 @@ INSERT INTO role (title, salary, department_id)
     VALUES ("Junior Engineer", 40000.00, 2);
 INSERT INTO role (title, salary, department_id)
     VALUES ("Lead Engineer", 150000.00, 2); 
+    
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Junior Support Tech", 35000.00, 3);
+INSERT INTO role (title, salary, department_id)
+    VALUES ("Lead Support Tech", 80000.00, 3); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES("Flora", "Aubergine", 4, NULL);
@@ -56,6 +62,11 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES("Random", "Maxima", 2, NULL);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES("Julian", "Redmond", 1, 3); 
+    VALUES("Julian", "Redmond", 1, 3);
+    
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES("Corwin", "Alexander", 6, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES("Merlin", "Wilbert", 5, 5); 
 
 
